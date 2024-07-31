7ba86e0c2f47f7234b7e71d2654ed21434c0cc3c
FojanI <90140319+FojanI@users.noreply.github.com>
2024-07-31 02:42:44 +0100

    input validation added to method1.sh

--- problem_1/method1.sh
+++ problem_1/method1.sh
@@ -1,4 +1,27 @@
-#!/usr/bin/bash
-
-ls *.$1 | cut -d"." -f 1 | xargs -I {} mv {}.$1 {}.$2
-
+#!/usr/bin/env bash
+ 
+# Function to display usage information
+usage() {
+    echo "Usage: $0 <source_extension> <target_extension>.This command line utility changes the file extension from <source_extension> to <target_extension>."
+    exit 1
+}
+ 
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Error: Invalid number of arguments. Please specify a source and destination extension."
+    usage
+fi
+ 
+# Assign input arguments to variables for better readability.
+source_ext="$1"
+target_ext="$2"
+ 
+ 
+# Check if any files with the source extension exist
+if ! ls *."$source_ext" >/dev/null 2>&1; then
+    echo "Error: No files found with the extension .$source_ext"
+    exit 1
+fi
+ 
+# Rename files with the given source extension to the target extension
+ls *."$source_ext" | cut -d"." -f 1 | xargs -I {} mv {}."$source_ext" {}."$target_ext"
